42181ff6b268d170bf5f3a1ade36dbeb84954699
timothybaraka <timothybaraka44@gmail.com>
2020-02-06 14:47:16 +0300

    Add a readme

--- readme.md
+++ readme.md
@@ -2,6 +2,10 @@
 This is a java app that enables a user to create a hero complete with the hero`s strenghts and weaknesses
 and also one can assign a hero to a group.
 
+### Author
+Timothy Baraka
+5 February 2020
+
 ### Getting Started
 Clone or download this repo into your machine to get started.
 
@@ -16,6 +20,7 @@ Spark - the Framework
 Intellij - IDE
 
 ### CodeBeat score
+[![codebeat badge](https://codebeat.co/badges/23bd2d50-7abf-47c7-8e3b-7695cc2ec52a)](https://codebeat.co/projects/github-com-timothybaraka-herosquad-dev)
 
 ### Licence
 There is no licence to this project.Fell free to conribute.
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,23 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 import static spark.route.HttpMethod.post;
 
 public class App {
-
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[]args){
+
+        port(getHerokuAssignedPort());
+
         staticFileLocation("/public");
 
         get("/", (request,response)-> {
