9941abb392eb8114075f38b3c04922037c7236b6
timothybaraka <timothybaraka44@gmail.com>
2020-02-06 22:05:16 +0300

    Add code code to connect to heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ import static spark.route.HttpMethod.post;
 public class App {
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
+        if (processBuilder.envionment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
